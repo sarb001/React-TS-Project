1e847e4b7e7c4b8fd93b011a76b32b2556e3b766
SARB <mrsinghbusiness05@gmail.com>
2023-09-26 10:46:22 +0530

    todo App - React + Ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,70 @@
 
-import { useState } from 'react';
-import './App.css';
-import { StateType  , increment , decrement , incrementByValue } from './assets/Components/Redux';
-import { useSelector , useDispatch } from 'react-redux/es/exports';
+// // import { useState } from 'react';
+// // import './App.css';
+// // import { StateType  , increment , decrement , incrementByValue } from './assets/Components/Redux';
+// // import { useSelector , useDispatch } from 'react-redux/es/exports';
 
 
-// import UseReducerUsage from './assets/Components/UseReducerUsage'
-// import ContextUsage from './assets/Components/ContextUsage'
-// import StateUsage from './assets/Components/StateUsage'
-// import Box from './assets/Components/Box'
+// // import UseReducerUsage from './assets/Components/UseReducerUsage'
+// // import ContextUsage from './assets/Components/ContextUsage'
+// // import StateUsage from './assets/Components/StateUsage'
+// // import Box from './assets/Components/Box'
 
-function App() {
+// function App() {
 
-  const [val, setval] = useState<number>(0);
-  const dispatch = useDispatch();
-  const count = useSelector((state : StateType ) => state.counter);
+//   // const [val, setval] = useState<number>(0);
+//   // const dispatch = useDispatch();
+//   // const count = useSelector((state : StateType ) => state.counter);
 
-   const incrementValueBy = () => {
-     setval(0)
-     dispatch(incrementByValue(val))
-   }
+//   //  const incrementValueBy = () => {
+//   //    setval(0)
+//   //    dispatch(incrementByValue(val))
+//   //  }
 
-   const incrementHandler = () => {
-     dispatch(increment());
-   }
-   const decrementHandler = () => {
-     dispatch(decrement());
-   }
+//   //  const incrementHandler = () => {
+//   //    dispatch(increment());
+//   //  }
+//   //  const decrementHandler = () => {
+//   //    dispatch(decrement());
+//   //  }
 
+//   return (
+//     <>
+//          <h1> Use React-Redux with Redux Toolkit  </h1>
+//           {/* <Box  heading = {"Hello Structuring here"} 
+//            addnumber = {45}
+//           /> */}
+//           {/* <StateUsage /> */}
+//           {/* <ContextUsage /> */}
+//           {/* <UseReducerUsage /> */}
+
+//           {/* <Redux /> */}
+
+
+//           {/* <div className = "mainapp">
+//                 <h3> Toolkit Counter here - {count}  </h3>
+//                   <button onClick = {incrementHandler}> Add ++ </button>
+//                   <button onClick = {decrementHandler}> Subtact --  </button>
+//                   <div>
+//                      <input type = 'number'  value = {val}   
+//                      onChange={(e) => setval(Number(e.target.value))} />
+//                   </div>
+//                   <button onClick = {incrementValueBy}> Add Value </button>
+//           </div> */}
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+// For React + Ts Project - Todo App  
+
+const App = () => {
   return (
     <>
-         <h1> Use React-Redux with Redux Toolkit  </h1>
-          {/* <Box  heading = {"Hello Structuring here"} 
-           addnumber = {45}
-          /> */}
-          {/* <StateUsage /> */}
-          {/* <ContextUsage /> */}
-          {/* <UseReducerUsage /> */}
-
-          {/* <Redux /> */}
-
-
-          <div className = "mainapp">
-                <h3> Toolkit Counter here - {count}  </h3>
-                  <button onClick = {incrementHandler}> Add ++ </button>
-                  <button onClick = {decrementHandler}> Subtact --  </button>
-                  <div>
-                     <input type = 'number'  value = {val}   
-                     onChange={(e) => setval(Number(e.target.value))} />
-                  </div>
-                  <button onClick = {incrementValueBy}> Add Value </button>
-          </div>
-
+        <h3> Inside the Todo App  </h3>
     </>
   )
 }
